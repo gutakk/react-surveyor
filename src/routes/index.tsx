7d27411ffbc17766077db75d575cf9e3c83eb2e2
Gut <thiramate@nimblehq.co>
2021-06-08 16:45:28 +0700

    [Task-5] Add home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+import Home from 'screens/Home'
 import SignIn from 'screens/SignIn'
 
 const Routes = (): JSX.Element => {
   return (
     <div>
       <Switch>
-        <Route path="/">{SignIn}</Route>
+        <Route path="/">{Home}</Route>
+        <Route path="/sign-in">{SignIn}</Route>
       </Switch>
     </div>
   )
