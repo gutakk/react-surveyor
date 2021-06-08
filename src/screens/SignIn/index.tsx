86151bde687ad1dd72d76c5bfe9b7ea9dd046ff0
Gut <thiramate@nimblehq.co>
2021-06-08 13:58:54 +0700

    [Task-5] Add sign in screen skeleton

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SignIn = (): JSX.Element => {
+  return (
+    <div className="app">
+      <p>Logo</p>
+      <p>Sign in to Nimble</p>
+      <div>Email</div>
+      <div>Password</div>
+      <button>Sign in</button>
+    </div>
+  )
+}
+
+export default SignIn
