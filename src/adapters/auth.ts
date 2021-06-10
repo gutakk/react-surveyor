5d03bfa725350c114c3f4d94cc78f33640c9ad54
Gut <thiramate@nimblehq.co>
2021-06-10 16:28:17 +0700

    [Task-5] Update auth adapter to make api call to nimble survey

--- src/adapters/auth.ts
+++ src/adapters/auth.ts
@@ -1,6 +1,24 @@
+import axios, { AxiosResponse } from 'axios'
+
 class AuthAdapter {
-  static signIn = (email: string, password: string) => {
-    return `hello ${email} and ${password}`
+  static signIn = (email: string, password: string): Promise<AxiosResponse> => {
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+    // prettier-ignore
+    return axios.post(
+      `${process.env.REACT_APP_NIMBLE_SURVEY_BASE_URL}/api/v1/oauth/token`,
+      {
+        'grant_type': 'password',
+        'email': email,
+        'password': password,
+        'client_id': process.env.REACT_APP_CLIENT_ID,
+        'client_secret': process.env.REACT_APP_CLIENT_SECRET
+      },
+      {
+        headers: headers
+      }
+    )
   }
 }
 
