7610c8e97a127c074f1d99169b71471f559e7ef5
Gut <thiramate@nimblehq.co>
2021-06-11 18:33:48 +0700

    [Task-6] Update class name

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -7,9 +7,9 @@ type BackgroundProps = {
 
 const Background = ({ backgroundImage, children }: BackgroundProps): JSX.Element => {
   return (
-    <div className="page-container">
-      <div className="page-container__background-image" style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <div className="page-container__overlay">{children}</div>
+    <div className="background">
+      <div className="background__image" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="background__overlay">{children}</div>
       </div>
     </div>
   )
