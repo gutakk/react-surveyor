714265e9c804687107d46da5c5a2e1122ace5e4e
Gut <thiramate@nimblehq.co>
2021-06-16 12:58:18 +0700

    [Task-6] Update context to follow the convention

--- cypress/integration/signIn.spec.ts
+++ cypress/integration/signIn.spec.ts
@@ -1,17 +1,17 @@
 describe('Sign In Page', () => {
-  describe('Given valid user credential', () => {
+  context('given valid user credential', () => {
     beforeEach(() => {
       cy.mockSignInResponse(200, 'signInResponse/valid_response.json')
       cy.signIn(Cypress.env('CYPRESS_VALID_EMAIL'), Cypress.env('CYPRESS_VALID_PASSWORD'))
     })
 
-    it('Redirects to home page', () => {
+    it('redirects to home page', () => {
       cy.get('button[type=submit]').click()
 
       cy.url().should('include', '/')
     })
 
-    it('Stores token to local storage', () => {
+    it('stores token to local storage', () => {
       cy.get('button[type=submit]').click().should(() => {
         expect(localStorage.getItem('access_token')).to.not.be.null
         expect(localStorage.getItem('refresh_token')).to.not.be.null
@@ -20,19 +20,19 @@ describe('Sign In Page', () => {
     })
   })
 
-  describe('Given invalid user credential', () => {
+  context('given invalid user credential', () => {
     beforeEach(() => {
       cy.mockSignInResponse(400, 'signInResponse/error_response.json')
       cy.signIn('invalid@email.com', 'invalidPassword')
     })
 
-    it('Does not redirect', () => {
+    it('does not redirect', () => {
       cy.get('button[type=submit]').click()
 
       cy.url().should('include', '/sign-in')
     })
 
-    it('Does not store token to local storage', () => {
+    it('does not store token to local storage', () => {
       cy.get('button[type=submit]').click().should(() => {
         expect(localStorage.getItem('access_token')).to.be.null
         expect(localStorage.getItem('refresh_token')).to.be.null
@@ -40,7 +40,7 @@ describe('Sign In Page', () => {
       })
     })
 
-    it('Displays error messages', () => {
+    it('displays error messages', () => {
       cy.get('button[type=submit]').click()
 
       cy.get('.alert').should('be.visible')
@@ -48,8 +48,8 @@ describe('Sign In Page', () => {
     })
   })
 
-  describe('Given forgot password clicked', () => {
-    it('Redirects to forgot password page', () => {
+  context('given forgot password clicked', () => {
+    it('redirects to forgot password page', () => {
       cy.visit('/sign-in')
       cy.get('.form-input-group__forgot-password').click()
 
