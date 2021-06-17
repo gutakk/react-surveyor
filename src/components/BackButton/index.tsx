8f6bebee5f1446d657f16af883e24b6ae61a4a25
Gut <thiramate@nimblehq.co>
2021-06-17 14:55:40 +0700

    [Task-7] Add back icon and unit test for BackButton

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 
+import backIcon from 'assets/images/icons/back.svg'
+
 const BackButton = (): JSX.Element => {
   const history = useHistory()
   const goBackFunc = () => history.goBack()
 
-  return <button onClick={goBackFunc}>Back</button>
+  return (
+    <button onClick={goBackFunc} data-testid="backButton">
+      <img src={backIcon} alt="back" />
+    </button>
+  )
 }
 
 export default BackButton
---
+++ src/tests/components/BackButton/index.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { render, RenderResult } from '@testing-library/react'
+
+import backIcon from 'assets/images/icons/back.svg'
+import BackButton from 'components/BackButton'
+
+describe('Given BackButton component is mounted', () => {
+  let element: RenderResult
+  beforeEach(() => {
+    element = render(<BackButton />)
+  })
+
+  it('renders the button', () => {
+    expect(element.getByTestId('backButton')).toBeInTheDocument()
+  })
+
+  it('renders back icon', () => {
+    const backImage = element.getByAltText('back')
+    expect(backImage).toBeInTheDocument()
+    expect(backImage).toHaveAttribute('src', backIcon)
+  })
+})
