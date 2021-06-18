3adc9a5abbaaf0000d681f6e9909233f492b1c0f
Gut <thiramate@nimblehq.co>
2021-06-18 17:31:59 +0700

    [Task-8] Add forgot password spec

---
+++ cypress/integration/ForgotPassword/forgotPassword.spec.ts
@@ -0,0 +1,34 @@
+import { contains } from 'cypress/types/jquery'
+import { formSelectors } from './selectors'
+
+describe('Forgot Password Page', () => {
+  context('given valid email', () => {
+    it('displays instruction message', () => {
+      cy.mockForgotPasswordResponse(200)
+      cy.forgotPassword(Cypress.env('CYPRESS_VALID_EMAIL'))
+
+      cy.findByTestId(formSelectors.alert).should('be.visible')
+      cy.findByTestId(formSelectors.alert).contains('We\'ve email you instructions to reset your password')
+    })
+  })
+
+  context('given invalid email', () => {
+    it('displays error message', () => {
+      cy.mockForgotPasswordResponse(400)
+      cy.forgotPassword('invalid@email.com')
+
+      cy.findByTestId(formSelectors.alert).should('be.visible')
+      cy.findByTestId(formSelectors.alert).contains('Invalid email')
+    })
+  })
+
+  context('given something went wrong', () => {
+    it('displays error message', () => {
+      cy.mockForgotPasswordResponse(500)
+      cy.forgotPassword(Cypress.env('CYPRESS_VALID_EMAIL'))
+
+      cy.findByTestId(formSelectors.alert).should('be.visible')
+      cy.findByTestId(formSelectors.alert).contains('Something went wrong. Please try again')
+    })
+  })
+})
