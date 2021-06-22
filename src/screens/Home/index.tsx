7c4670c8ef93172a4c28dd39a4a58c4f35549523
Gut <thiramate@nimblehq.co>
2021-06-22 15:51:29 +0700

    [Task-9] Add function to delay the loading

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import LazyLoader from 'components/LazyLoader'
 
 const Home = (): JSX.Element => {
-  return (
-    <div>
-      {/* Hello from Home page */}
-      <LazyLoader />
-    </div>
-  )
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  })
+
+  return <div>{isLoading ? <LazyLoader /> : 'Hello from Home page'}</div>
 }
 
 export default Home
