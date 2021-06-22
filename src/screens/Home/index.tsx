604726fd93d3ce05cef6a1fc1210e938aa14ee00
Gut <thiramate@nimblehq.co>
2021-06-22 16:54:34 +0700

    [Task-9] Remove delay logic

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import LazyLoader from 'components/LazyLoader'
 
 const Home = (): JSX.Element => {
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 2000)
-  })
-
-  return <div>{isLoading ? <LazyLoader /> : 'Hello from Home page'}</div>
+  return <LazyLoader />
 }
 
 export default Home
