f5453e75e0b046e2843660dfa58e388b5b4804f7
Gut <thiramate@nimblehq.co>
2021-06-22 14:50:15 +0700

    [Task-9] Update style

--- src/components/LazyLoader/index.tsx
+++ src/components/LazyLoader/index.tsx
@@ -6,42 +6,44 @@ const LazyLoader = (): JSX.Element => {
   const highlightColor = '#49494D'
 
   return (
-    <SkeletonTheme color={themeColor} highlightColor={highlightColor}>
-      <div className="lazy-loader-header">
-        <span className="lazy-loader-header__web-icon">
-          <Skeleton />
-        </span>
-        <span className="lazy-loader-header__user-image">
-          <Skeleton />
-        </span>
-      </div>
-
-      <div className="lazy-loader-body">
-        <span className="lazy-loader-body__date">
-          <Skeleton />
-        </span>
-        <span className="lazy-loader-body__day">
-          <Skeleton />
-        </span>
-        <span className="lazy-loader-body__image">
-          <Skeleton />
-        </span>
-
-        <div className="lazy-loader-footer">
-          <div className="lazy-loader-footer__survey-info-container">
-            <span className="lazy-loader-footer__title">
-              <Skeleton />
-            </span>
-            <span className="lazy-loader-footer__subtitle">
-              <Skeleton />
-            </span>
-          </div>
-          <span className="lazy-loader-footer__button">
+    <div className="container-lazy-loader">
+      <SkeletonTheme color={themeColor} highlightColor={highlightColor}>
+        <div className="lazy-loader-header">
+          <span className="lazy-loader-header__web-icon">
+            <Skeleton />
+          </span>
+          <span className="lazy-loader-header__user-image">
             <Skeleton />
           </span>
         </div>
-      </div>
-    </SkeletonTheme>
+
+        <div className="lazy-loader-body">
+          <span className="lazy-loader-body__date">
+            <Skeleton />
+          </span>
+          <span className="lazy-loader-body__day">
+            <Skeleton />
+          </span>
+          <span className="lazy-loader-body__image">
+            <Skeleton />
+          </span>
+
+          <div className="lazy-loader-footer">
+            <div className="lazy-loader-footer__survey-info-container">
+              <span className="lazy-loader-footer__title">
+                <Skeleton />
+              </span>
+              <span className="lazy-loader-footer__subtitle">
+                <Skeleton />
+              </span>
+            </div>
+            <span className="lazy-loader-footer__button">
+              <Skeleton />
+            </span>
+          </div>
+        </div>
+      </SkeletonTheme>
+    </div>
   )
 }
 
