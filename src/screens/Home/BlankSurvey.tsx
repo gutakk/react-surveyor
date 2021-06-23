9c63d162882d9870b5ffa7050df890ea9751327f
Gut <thiramate@nimblehq.co>
2021-06-23 15:03:24 +0700

    [Task-10] Update blank survey component

--- src/screens/Home/BlankSurvey.tsx
+++ src/screens/Home/BlankSurvey.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import sunglassesEmo from 'assets/images/sunglasses-emo.png'
+
 const BlankSurvey = (): JSX.Element => {
   return (
     <div className="blank-survey" data-test-id="blankSurvey">
-      Hello from blank survey
+      <div className="blank-survey-header">
+        <p className="blank-survey-header__date">MONDAY, JUNE 15</p>
+        <p className="blank-survey-header__day">Today</p>
+      </div>
+      <div className="blank-survey-detail">
+        <img className="blank-survey-detail__emo" src={sunglassesEmo} alt="Sunglasses" />
+        <p className="blank-survey-detail__message">You’ve completed all the survey.</p>
+        <p className="blank-survey-detail__message">Take a moment.</p>
+      </div>
     </div>
   )
 }
