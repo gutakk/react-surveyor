741619753f8b6958848c295ee1485e46b6bfddb6
Gut <thiramate@nimblehq.co>
2021-06-23 17:45:47 +0700

    [Task-10] Update file name

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import SurveyList from 'screens/Home/SurveyList'
+import SurveyList from 'screens/Home/surveyList'
 
 // TODO: Add lazy loader when fetch the survey
 const Home = (): JSX.Element => {
