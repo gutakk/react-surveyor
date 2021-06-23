f76de39a1bf7288febdd2350f37e5e7520752afb
Gut <thiramate@nimblehq.co>
2021-06-23 15:57:51 +0700

    [Task-10] Get today date

--- src/components/SurveyTime/index.tsx
+++ src/components/SurveyTime/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const SurveyTime = (): JSX.Element => {
+  const options: Intl.DateTimeFormatOptions = { weekday: 'long', month: 'long', day: 'numeric' }
+  const today: string = new Date().toLocaleString('en-EN', options)
+
   return (
     <div className="survey-time">
-      <p className="survey-time__date">Monday, June 15</p>
+      <p className="survey-time__date">{today}</p>
       <p className="survey-time__day">Today</p>
     </div>
   )
