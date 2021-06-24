a1ff57db79124fc965102f4473e998e7ae9595fd
Gut <thiramate@nimblehq.co>
2021-06-24 13:59:47 +0700

    [Task-11] Add surveyList screen

---
+++ src/screens/Home/surveyList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SurveyList = (): JSX.Element => {
+  return (
+    <div className="survey-list" data-test-id="surveyList">
+
+    </div>
+  )
+}
+
+export default SurveyList
