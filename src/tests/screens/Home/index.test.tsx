820d16abfb695f9baab31d14603448a108b4e914
Gut <thiramate@nimblehq.co>
2021-06-24 12:35:16 +0700

    [Task-10] Update test case name

--- src/tests/screens/Home/index.test.tsx
+++ src/tests/screens/Home/index.test.tsx
@@ -5,7 +5,7 @@ import { render } from '@testing-library/react'
 import Home from 'screens/Home'
 
 describe('given Home page is mounted', () => {
-  it('renders survey list', () => {
+  it('renders survey', () => {
     const { getByTestId } = render(<Home />)
     const survey = getByTestId('survey')
 
