2e93c2106bcb09a2f9be11cc8a347d45f6486747
Gut <thiramate@nimblehq.co>
2021-06-25 13:28:11 +0700

    [Task-11] Add graphql client

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
 import Survey from 'screens/Home/survey'
 
 // TODO: Add lazy loader when fetch the survey
 const Home = (): JSX.Element => {
-  return <Survey />
+  const httpLink = new HttpLink({
+    uri: 'https://nimble-survey-web-staging.herokuapp.com/graphql'
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('access_token')
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    }
+  })
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <Survey />
+    </ApolloProvider>
+  )
 }
 
 export default Home
--- src/screens/Home/surveyList.tsx
+++ src/screens/Home/surveyList.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SurveyList = (): JSX.Element => {
   return (
     <div className="survey-list" data-test-id="surveyList">
-
+      <p>hello from survey list</p>
     </div>
   )
 }
