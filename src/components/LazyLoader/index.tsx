73b1b7f862381c5a33eca0d754ab7534a686f7b3
Gut <thiramate@nimblehq.co>
2021-06-28 16:13:37 +0700

    [Task-9] Update lazyloader class

--- src/components/LazyLoader/index.tsx
+++ src/components/LazyLoader/index.tsx
@@ -6,38 +6,38 @@ const LazyLoader = (): JSX.Element => {
   const highlightColor = '#49494D'
 
   return (
-    <div className="container-lazy-loader" data-test-id="lazyLoader">
+    <div className="lazy-loader" data-test-id="lazyLoader">
       <SkeletonTheme color={themeColor} highlightColor={highlightColor}>
-        <div className="lazy-loader-header">
-          <span className="lazy-loader-header__web-icon" data-test-id="webIconLazyLoader">
+        <div className="lazy-loader__header">
+          <span className="lazy-loader__web-icon" data-test-id="webIconLazyLoader">
             <Skeleton />
           </span>
-          <span className="lazy-loader-header__user-image" data-test-id="userImageLazyLoader">
+          <span className="lazy-loader__user-image" data-test-id="userImageLazyLoader">
             <Skeleton />
           </span>
         </div>
 
-        <div className="lazy-loader-body">
-          <span className="lazy-loader-body__date" data-test-id="dateLazyLoader">
+        <div className="lazy-loader__body">
+          <span className="lazy-loader__date" data-test-id="dateLazyLoader">
             <Skeleton />
           </span>
-          <span className="lazy-loader-body__day" data-test-id="dayLazyLoader">
+          <span className="lazy-loader__day" data-test-id="dayLazyLoader">
             <Skeleton />
           </span>
-          <span className="lazy-loader-body__survey-image" data-test-id="surveyImageLazyLoader">
+          <span className="lazy-loader__survey-image" data-test-id="surveyImageLazyLoader">
             <Skeleton />
           </span>
 
-          <div className="lazy-loader-footer">
-            <div className="lazy-loader-footer__survey-info-container">
-              <span className="lazy-loader-footer__survey-title" data-test-id="surveyTitleLazyLoader">
+          <div className="lazy-loader__footer">
+            <div className="lazy-loader__survey-info-container">
+              <span className="lazy-loader__survey-title" data-test-id="surveyTitleLazyLoader">
                 <Skeleton />
               </span>
-              <span className="lazy-loader-footer__survey-subtitle" data-test-id="surveySubtitleLazyLoader">
+              <span className="lazy-loader__survey-subtitle" data-test-id="surveySubtitleLazyLoader">
                 <Skeleton />
               </span>
             </div>
-            <span className="lazy-loader-footer__button" data-test-id="buttonLazyLoader">
+            <span className="lazy-loader__button" data-test-id="buttonLazyLoader">
               <Skeleton />
             </span>
           </div>
