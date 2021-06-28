400862d643d8bb4a4a643afd4da770ca6183042d
Gut <thiramate@nimblehq.co>
2021-06-28 12:27:50 +0700

    [Task-8] Move forgot password validation UI test to main file

--- cypress/integration/ForgotPassword/forgotPassword.spec.ts
+++ cypress/integration/ForgotPassword/forgotPassword.spec.ts
@@ -39,4 +39,15 @@ describe('Forgot Password Page', () => {
       cy.url().should('include', '/sign-in')
     })
   })
+
+  context('given empty email', () => {
+    it('displays form validation message', () => {
+      cy.visit('/forgot-password')
+
+      cy.findByTestId(formSelectors.button).click()
+
+      cy.findByTestId(formSelectors.alert).should('be.visible')
+      cy.findByTestId(formSelectors.alert).contains('Email is required')
+    })
+  })
 })
