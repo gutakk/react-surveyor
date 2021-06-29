11adf5efac780dfc7690c9007f467e36dc8784ed
Gut <thiramate@nimblehq.co>
2021-06-29 17:24:20 +0700

    [Task-11] Add React Slick

--- src/screens/Home/surveyList.tsx
+++ src/screens/Home/surveyList.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import React from 'react'
+import Slider from 'react-slick'
 
 import nextIcon from 'assets/images/icons/next.svg'
 
@@ -15,25 +17,38 @@ type SurveyListProps = {
   surveyList: Array<SurveyData>
 }
 
+const slickSettings = {
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  autoplay: true,
+  speed: 500
+}
+
 const SurveyList = ({ surveyList }: SurveyListProps): JSX.Element => {
   return (
     <div className="survey-list" data-test-id="surveyList">
-      {surveyList.map((survey) => {
-        return (
-          <div key={survey.node.id} className="survey-list-data">
-            <img src={survey.node.coverImageUrl} alt={survey.node.title} className="survey-list-data__image" />
-            <div className="survey-list-data__footer">
-              <div className="survey-list-data__info-container">
-                <p className="survey-list-data__title">{survey.node.title}</p>
-                <p className="survey-list-data__description">{survey.node.description}</p>
-              </div>
-              <div className="survey-list-data__next">
-                <img src={nextIcon} alt="Next" />
+      <Slider {...slickSettings}>
+        {surveyList.map((survey) => {
+          return (
+            <div key={survey.node.id} className="survey-list-data">
+              <img src={survey.node.coverImageUrl} alt={survey.node.title} className="survey-list-data__image" />
+              <div className="survey-list-data__footer">
+                <div className="survey-list-data__info-container">
+                  <p className="survey-list-data__title">{survey.node.title}</p>
+                  <p className="survey-list-data__description">{survey.node.description}</p>
+                </div>
+                <div className="survey-list-data__next">
+                  <img src={nextIcon} alt="Next" />
+                </div>
               </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </Slider>
     </div>
   )
 }
