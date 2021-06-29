e6d4a8b9f08613ed8cf93f32d145c6d68a240fd8
Gut <thiramate@nimblehq.co>
2021-06-29 18:04:46 +0700

    [Task-11] Update survey unit test

--- src/tests/screens/Home/survey.test.tsx
+++ src/tests/screens/Home/survey.test.tsx
@@ -5,29 +5,73 @@ import { render } from '@testing-library/react'
 import Survey from 'screens/Home/survey'
 
 describe('given survey page is mounted', () => {
-  describe('given surveyList', () => {
+  describe('given valid survey list', () => {
     it('renders survey header', () => {
-      // const surveyList = [1]
-      // const { getByTestId } = render(<Survey surveyList={surveyList} />)
-      // const surveyHeader = getByTestId('surveyHeader')
+      const surveyList = [{
+        node: {
+          id: '1',
+          title: 'title-1',
+          description: 'description-1',
+          coverImageUrl: 'image-1'
+        }
+      }]
+      const { getByTestId } = render(<Survey surveyList={surveyList} />)
+      const surveyHeader = getByTestId('surveyHeader')
     
-      expect(true)
+      expect(surveyHeader).toBeInTheDocument()
     })
     
-    // it('renders survey time', () => {
-    //   const surveyList = [1]
-    //   const { getByTestId } = render(<Survey surveyList={surveyList} />)
-    //   const surveyTime = getByTestId('surveyTime')
+    it('renders survey time', () => {
+      const surveyList = [{
+        node: {
+          id: '1',
+          title: 'title-1',
+          description: 'description-1',
+          coverImageUrl: 'image-1'
+        }
+      }]
+      const { getByTestId } = render(<Survey surveyList={surveyList} />)
+      const surveyTime = getByTestId('surveyTime')
     
-    //   expect(surveyTime).toBeInTheDocument()
-    // })
+      expect(surveyTime).toBeInTheDocument()
+    })
     
-    // it('renders blank survey', () => {
-    //   const surveyList = [1]
-    //   const { getByTestId } = render(<Survey surveyList={surveyList} />)
-    //   const blankSurvey = getByTestId('blankSurvey')
+    it('renders surveyList', () => {
+      const surveyList = [{
+        node: {
+          id: '1',
+          title: 'title-1',
+          description: 'description-1',
+          coverImageUrl: 'image-1'
+        }
+      }]
+      const { getByTestId } = render(<Survey surveyList={surveyList} />)
+      const surveyListPage = getByTestId('surveyList')
     
-    //   expect(blankSurvey).toBeInTheDocument()
-    // })
+      expect(surveyListPage).toBeInTheDocument()
+    })
+  })
+
+  describe('given empry survey list', () => {
+    it('renders survey header', () => {
+      const { getByTestId } = render(<Survey surveyList={[]} />)
+      const surveyHeader = getByTestId('surveyHeader')
+    
+      expect(surveyHeader).toBeInTheDocument()
+    })
+    
+    it('renders survey time', () => {
+      const { getByTestId } = render(<Survey surveyList={[]} />)
+      const surveyTime = getByTestId('surveyTime')
+    
+      expect(surveyTime).toBeInTheDocument()
+    })
+    
+    it('renders blankSurvey', () => {
+      const { getByTestId } = render(<Survey surveyList={[]} />)
+      const blankSurvey = getByTestId('blankSurvey')
+    
+      expect(blankSurvey).toBeInTheDocument()
+    })
   })
 })
