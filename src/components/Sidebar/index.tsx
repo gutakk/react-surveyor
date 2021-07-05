a4e9af6241b8a08e1c65a23300cff46fc2fa6617
Gut <thiramate@nimblehq.co>
2021-07-05 15:25:38 +0700

    [Task-13] Add sidebar content

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,7 +7,11 @@ type SidebarProps = {
 const Sidebar = ({ isOpen }: SidebarProps): JSX.Element => {
   return (
     <div className={`${isOpen ? 'sidebar sidebar--active' : 'sidebar'}`} data-test-id="sidebar">
-      <span></span>
+      <div className="sidebar__content">
+        <p className="sidebar__user">User</p>
+        <hr />
+        <p className="sidebar__choice">Logout</p>
+      </div>
     </div>
   )
 }
