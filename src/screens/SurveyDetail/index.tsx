ddd4b21c62c7c0906829b3e15a2d769ee29eb38e
Gut <thiramate@nimblehq.co>
2021-07-06 18:14:39 +0700

    [Task-15] Fetch survey detail

--- src/screens/SurveyDetail/index.tsx
+++ src/screens/SurveyDetail/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-const SurveyDetail = (): JSX.Element => {
-  const x = useParams()
-  console.log(x)
+import { gql, useQuery } from '@apollo/client'
 
-  return <div>Hello from survey detail</div>
+import LazyLoader from 'components/LazyLoader'
+
+type SurveyDetailParams = {
+  surveyID: string
+}
+
+const SurveyDetail = (): JSX.Element => {
+  const { surveyID } = useParams<SurveyDetailParams>()
+  const GET_SURVEY_DETAIL = gql`
+  query Survey {
+    survey(id: "${surveyID}") {
+      id
+      title
+      description
+      coverImageUrl
+    }
+  }
+  `
+
+  const { data, loading, error } = useQuery(GET_SURVEY_DETAIL)
+  if (loading) return <LazyLoader />
+  if (error?.networkError?.message.includes('401')) {
+    // TODO: Redirect to home page and clear local storage (this still not working because of REFRESH action bug)
+    return <p>Unauthorized</p>
+  }
+  if (error) {
+    // TODO: Create something went wrong screen
+    return <p>Something went wrong</p>
+  }
+
+  return <div>{data.survey.title}</div>
 }
 
 export default SurveyDetail
