1580844d08919269f42e2a274218ef8fd25ac129
Gut <thiramate@nimblehq.co>
2021-07-06 18:26:04 +0700

    [Task-12] Update survey list to use background component

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 type BackgroundProps = {
   backgroundImage: string
   pageClassName: string
+  context: string
   children: JSX.Element | JSX.Element[]
 }
 
-const Background = ({ backgroundImage, pageClassName, children }: BackgroundProps): JSX.Element => {
+const Background = ({ backgroundImage, pageClassName, context, children }: BackgroundProps): JSX.Element => {
   return (
     <div className={`${pageClassName} background`}>
       <div
@@ -14,7 +15,7 @@ const Background = ({ backgroundImage, pageClassName, children }: BackgroundProp
         style={{ backgroundImage: `url(${backgroundImage})` }}
         data-test-id="backgroundImage"
       >
-        <div className="background__overlay">{children}</div>
+        <div className={`background__overlay background__overlay--${context}`}>{children}</div>
       </div>
     </div>
   )
